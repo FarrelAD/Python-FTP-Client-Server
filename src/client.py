afd5afc903c5f72be2df8ed0473e32558441ca8f
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-03-29 11:24:59 +0700

    add: connection authentication and closing connection

--- src/client.py
+++ src/client.py
@@ -1,4 +1,6 @@
+from colorama import Fore
 from command import Command
+import os
 import socket
 import questionary
 
@@ -7,39 +9,110 @@ HOST = 'localhost'
 PORT = 21
 
 client_socket = None
+is_connection_active = False
 
 def menu() -> None:
-    answer = questionary.select(
-        "Choose menu",
-        choices=[
-            "Start FTP client",
-            ".2.",
-            ".3."
-        ]).ask()
+    while True:
+        os.system('clear')
+        
+        if is_connection_active:
+            connection_status = f"{Fore.GREEN}Active {client_socket.getpeername()}{Fore.RESET}"
+        else:
+            connection_status = Fore.RED + "Not Active" + Fore.RESET
+        
+        print(f"Connection status: {connection_status}")
+        
+        answer = questionary.select(
+            "Choose menu",
+            choices=[
+                "Start FTP client",
+                "Exit"
+            ]).ask()
 
-    match answer:
-        case 'Start FTP client':
-            start()
-        case _:
-            print("You choose unknown menu! Program ended!")
+        match answer:
+            case "Start FTP client":
+                start()
+            case "Exit":
+                print(f"{Fore.GREEN}Bye{Fore.RESET}")
+                break
 
 def start() -> None:
-    global client_socket
+    global client_socket, is_connection_active
     
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.bind((HOST, PORT))
+    answers = questionary.form(
+        input_host=questionary.text("Input server address"),
+        input_port=questionary.text("Input server port")
+    ).ask()
+    
+    HOST = answers['input_host']
+    PORT = int(answers['input_port'])
+    
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((HOST, PORT))
+        
+        is_connection_active = True
+        
+        response = client_socket.recv(1024).decode()
+        print(f"{Fore.GREEN}Response: {response}{Fore.RESET}")
+        
+        authenticate_user()
+        questionary.press_any_key_to_continue("Press any key to Continue >").ask()
+        run_communication()
+    except ConnectionRefusedError:
+        print(f"{Fore.RED}Error: Server is not available. Please try again later.{Fore.RESET}")
+    except Exception as e:
+        print(f"{Fore.RED}Unexpected error: {e}{Fore.RESET}")
+    finally:
+        client_socket.close()
+        is_connection_active = False
+    
+    questionary.press_any_key_to_continue("Press any key to Continue >").ask()
 
-def send_command(command: Command, arg: str = None) -> None:
-    global client_socket
-    
-    print(f"Sending command: {command} {'with argument: ' + arg if arg is not None else ''}")
-    
-    full_command = command.name + (arg or '')
+def send_command(command: Command, arg: str = '') -> None:
+    full_command = f"{command.name} {arg}"
     client_socket.sendall(full_command.encode() + b'\r\n')
     
     response = client_socket.recv(1024).decode()
     print(f"Response: {response}")
     return response
+
+def run_communication() -> None:
+    while True:
+        os.system('clear')
+        
+        answer = questionary.select(
+            "Choose command",
+            choices=[
+                "Send file",
+                "Print working directory",
+                "Change working directory",
+                "List files and directories",
+                "Create directory",
+                "Remove directory",
+                "Download file from the server",
+                "Upload file to the server"
+                "Delete file",
+                "Exit"
+            ]
+        ).ask()
+        
+        match answer:
+            case "Exit":
+                close_connection()
+                break
+
+def authenticate_user() -> None:
+    answers = questionary.form(
+        input_username=questionary.text("Input username"),
+        input_password=questionary.text("Input password")
+    ).ask()
+    
+    send_command(Command.USER, answers['input_username'])
+    send_command(Command.PASS, answers['input_password'])
+
+def close_connection() -> None:
+    send_command(Command.QUIT)
 
 def main() -> None:
     menu()
--- src/server.py
+++ src/server.py
@@ -1,8 +1,10 @@
-import questionary
+from colorama import Fore
+from command import Command
 import os
 import select
 import socket
 import threading
+import questionary
 
 
 HOST = 'localhost'
@@ -67,9 +69,59 @@ def start():
         server_socket.close()
         
 def handle_client(client_socket: socket.socket) -> None:
-    command = client_socket.recv(1024).decode().strip()
+    client_socket.sendall(b"220 Service ready for new user.\r\n")
     
-    print(f"Received command: {command}")
+    while True:
+        client_command = client_socket.recv(1024).decode().strip()
+        
+        print(f"Received command: {client_command}")
+        
+        command_start = client_command.split()[0]
+        
+        match command_start:
+            case Command.USER.name:
+                handle_user_command(client_socket)
+            case Command.PASS.name:
+                handle_pass_command(client_socket)
+            case Command.PASV.name:
+                passive_mode()
+            case Command.LIST.name:
+                listing_directory()
+            case Command.RETR.name:
+                retrieving_file()
+            case Command.QUIT.name:
+                close_client_connection(client_socket)
+                break
+            case _:
+                client_socket.sendall(b"502 Command not implemented\r\n")
+
+def handle_user_command(client_socket: socket.socket) -> None:
+    client_socket.sendall(b"331 User name okay, need password.")
+
+def handle_pass_command(client_socket: socket.socket) -> None:
+    client_socket.sendall(b"230 User logged in, proceed.")
+
+def authenticate() -> None:
+    pass
+
+def passive_mode() -> None:
+    pass
+
+def listing_directory() -> None:
+    pass
+
+def retrieving_file() -> None:
+    pass
+
+def uploading_file() -> None:
+    pass
+
+def close_client_connection(client_socket: socket.socket) -> None:
+    print(f"{Fore.RED}Connection closed with {client_socket.getpeername()}{Fore.RESET}")
+    client_socket.sendall(b"221 Good bye.")
+    client_socket.close()
+
+
 
 def main() -> None:
     menu()
